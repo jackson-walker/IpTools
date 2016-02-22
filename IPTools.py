6b719b427ff1635024a1c6eef402a52f465ef90c
Jackson Walker <cheezcakep@gmail.com>
2016-02-22 13:56:17 -0600

    Inital Commit
    swag

---
+++ IPTools.py
@@ -0,0 +1,23 @@
+import os
+
+os.system("clear")
+
+print "##############################"
+print "IP Tools by Jackson Walker"
+print "Enter IP address and subnet mask"
+print "You will then see output of class, subnet, host"
+print "############################## \n"
+
+sIP = input("Please input IP address to be examined: ")
+sSubnet = input("Please input subnet mask for this address: ")
+
+
+def convertBinary(str):
+	"This takes a string in TCP/IP format and returns a binary number of the TCP/IP format number"
+	sIpSplit = str.split('.')
+	
+	
+def toBinary(str):
+	"This takes a split up string and converts that to a binary number"
+	
+
